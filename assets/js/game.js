21c915001393fc7826af9481793be729564877e7
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-03-22 10:56:49 -0400

    problem

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,8 +19,11 @@ var enemyAttack = 12;
 
 /*--------Fight function (before the "for" loop because it will be called inside the loop) ------------------------------------------------------------------*/
 var fight = function(enemyName) {
+  // repeat and execute as long as the enemy-robot is alive
+  while(enemyHealth > 0) {
+    
   // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+  // window.alert("Welcome to Robot Gladiators!");
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -72,10 +75,15 @@ var fight = function(enemyName) {
     window.alert("You need to pick a valid option. Try again!");
   }
 };
+  }
 
 /*------------------------------ Entering the "for" loop into the game ------------------------------------------------------------------*/
-for(var i = 0; i <enemyNames.length; i++) {
-  fight(enemyNames[i]);
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
 
-// i'm in develop
+
+
+// i'm in develop branch

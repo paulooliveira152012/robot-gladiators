d308da1e0a68957aed1d85f51c56281becbda711
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-03-24 13:14:19 -0400

    one more big step accomplished

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,8 +15,60 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var shop = function() {
-  console.log("entered the shop");
-  window.alert("You have entered the shop")
+
+  window.alert('you have $ ' + playerMoney + " to spend!");
+  console.log("available money: $" + playerMoney);
+
+  // ask player what they'd like to do
+  var shopOptionPrompt = window.prompt(
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  );
+
+  // use switch to carry out action
+switch (shopOptionPrompt) {
+  case "refill":
+  case "REFILL": //new case
+
+    if (playerMoney >= 7) {
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+  
+      // increase health and decrease money
+      playerHealth = playerHealth + 20;
+      playerMoney = playerMoney - 7;
+    }
+    else {
+      window.alert("You don't have enough money!");
+    }
+  
+    break;
+  case "upgrade":
+  case "UPGRADE":
+    if (playerMoney >= 7) {
+      window.alert("Upgrading player's attack by 6 for 7 dollars.");
+  
+     // increase attack and decrease money
+      playerAttack = playerAttack + 6;
+      playerMoney = playerMoney - 7;
+    }
+    else {
+      window.alert("You don't have enough money!");
+    }
+  
+    break;
+  
+  case "leave":
+  case "LEAVE":
+    window.alert("Leaving the store.");
+
+    // do nothing, so function will end
+    break;
+  default:
+    window.alert("You did not pick a valid option. Try again.");
+
+    // call shop() again to force player to pick a valid option
+    shop();
+    break;
+}
 };
 
 /*--------Fight function (before the "for" loop because it will be called inside the loop) ------------------------------------------------------------------*/
@@ -62,8 +114,6 @@ var fight = function (enemyName) {
       // award player money for winning
       playerMoney = playerMoney + 20;
 
-      shop()
-
       // leave while() loop since enemy is dead
       //  break; breakpoint no longer needed
     } else {
@@ -96,6 +146,7 @@ var fight = function (enemyName) {
 /*------------------------------ Entering the "for" loop into the game ------------------------------------------------------------------*/
 /*------------------------------ fight each enemy-robot by looping over them and fighting them one at a time ----------------------------*/
 //function to start the game
+
 var startGame = function () { //3rd (loops for each player)
   // reset player stats
   playerHealth = 100;
@@ -117,6 +168,18 @@ var startGame = function () { //3rd (loops for each player)
     enemyHealth = 50;
 
     fight(pickedEnemyName[i]);
+
+    // if we're not at the last enemy in the array
+    // if player is still alive and we're not at the last enemy in the array
+    if (playerHealth > 0 && i < pickedEnemyName.length - 1) {
+      // ask if player wants to use the store before next round
+      var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+    
+      // if yes, take them to the store() function
+      if (storeConfirm) {
+        shop();
+      }
+    }
 }
 
   // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
@@ -151,6 +214,9 @@ var endGame = function () {
 // start the game when the page loads
 startGame(); //2nd
 
+
+
+
 /*------------------adding mwindow alert to see how you did during the game------------------------------------------------------*/
 var endGame = function() {
   // if player is still alive, player wins!
@@ -183,7 +249,11 @@ else {
 the game at all, instead, the variables at variable "startGame" (which i assume would apply 
   only when restarting the game after it has ended) are?
 
-1. Why am I still being attacked by the the enemy even after he has been defeated?
+2. Why am I still being attacked by the the enemy even after he has been defeated?
+
+3. On "check enemy's health" instead of making the attack value = null after oponent has died
+I want to prevent the attack from happening at all
+
 */
 
 
